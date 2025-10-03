a1d74eabb6444a890b9a3fbc253e6952754b359d
LucaCattaneoo <l.cattaneo21@studenti.unibg.it>
2025-10-03 11:28:44 +0200

    statistiche complete

--- codice/index.php
+++ codice/index.php
@@ -37,12 +37,18 @@ include 'header.php';
             $result_libri_count = $conn->query($sql_libri_count);
             
             $total_libri = ($result_libri_count && $result_libri_count->num_rows > 0) ? $result_libri_count->fetch_assoc()['total_libri'] : 0;
-            echo '<div class="stat-card"><h3>Libri Totali</h3><p class="stat-number">' . $total_libri . '</p></div>';
+            echo '<div class="stat-card"><h3>Ricettari totali</h3><p class="stat-number">' . $total_libri . '</p></div>';
             $sql_ricette_count = "SELECT COUNT(*) AS total_ricette FROM Ricetta";
             $result_ricette_count = $conn->query($sql_ricette_count);
             $total_ricette = ($result_ricette_count && $result_ricette_count->num_rows > 0) ? $result_ricette_count->fetch_assoc()['total_ricette'] : 0;
-            echo '<div class="stat-card"><h3>Ricette Totali</h3><p class="stat-number">' . $total_ricette . '</p></div>';
-            echo '<div class="stat-card"><h3>da decidere</h3><p class="stat-number">' . $total_libri . '</p></div>';            ?>
+            echo '<div class="stat-card"><h3>Ricette totali</h3><p class="stat-number">' . $total_ricette . '</p></div>';
+            
+            
+            $sql_ingredienti = "SELECT COUNT(*) AS total FROM Ingrediente";
+            $result_ingredienti = $conn->query($sql_ingredienti);
+            $total_ingredienti = ($result_ingredienti && $result_ingredienti->num_rows > 0) ? $result_ingredienti->fetch_assoc()['total'] : 0;
+            echo '<div class="stat-card"><h3>Ingredienti unici</h3><p class="stat-number">'. $total_ingredienti . '</p></div>';
+           ?>
         </div>
     </section>
 </main>
